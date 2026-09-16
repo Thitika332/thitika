f2711056625e6878e51094e1327130020789f3f8
Thitika332 <thitika9252@gmail.com>
2026-09-16 11:45:39 +0700

    Correct triangle area calculation in Examination_1.py
    Fix variable names and calculate triangle area correctly.

--- Examination_1.py
+++ Examination_1.py
@@ -7,8 +7,4 @@
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
 b = float(input(ความยาวฐาน))
 h = float(input(ความสูง))
-num1 = int(input("b: "))
-num2 = int(input("h: "))
-
-total = num1 * nam2 * 0.5
-print = ("พื้นที่สามเหลี่ยม : ", total)
+print = (0.5 * b * h)
